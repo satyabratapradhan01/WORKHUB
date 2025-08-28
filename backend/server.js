4500c1795466638d2ff1878b697fb02ce40e8504
satyabratapradhan <satyabratapradhann@gmail.com>
2025-08-28 14:01:19 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello Satya...")
+})
+
+app.listen(port , ()=>{
+    console.log(`Server started on PORT ${port}`);
+});
